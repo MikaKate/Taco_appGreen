ee985cf006c8fe7b3bbdfe18c41e16bc0bb852e0
MikaKate <kaelakate02@gmail.com>
2023-03-24 11:17:26 +0100

    Initial Commit

---
+++ esercizio/esercizioApp.swift
@@ -0,0 +1,17 @@
+//
+//  esercizioApp.swift
+//  esercizio
+//
+//  Created by Micaela on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct esercizioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
