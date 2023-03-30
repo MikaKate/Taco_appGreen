8f16cda5d9fceb494b5e6842e0b60ecb087c0345
MikaKate <kaelakate02@gmail.com>
2023-03-30 11:34:23 +0200

    Update ContentView.swift

--- esercizio/ContentView.swift
+++ esercizio/ContentView.swift
@@ -8,29 +8,125 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedOption = 0
+    //variabili per la pagina "liste"
+    let optionsList = ["Recent", "Categories"]
+
+    //variabili per la pagina "prodotti"
+    let optionsProducts = ["Low Risk", "All", "High Risk"]
+    @State private var text = ""
+    
+    //variabili per la pagina "per te"
+    let optionsForYou = ["For You", "More"]
+    
     var body: some View {
         TabView {
-            NavigationView(){
-                List {
-                    NavigationLink("Lista 1", destination: Lista_1())
-                    Text("Name")
+            //Pagina delle liste
+            VStack{
+                HStack {
+                    Button(action: {
+                                // Azione del bottone
+                    }){
+                        Image(systemName: "magnifyingglass")
+                    }
+                    Spacer()
+                    Button(action: {
+                                // Azione del bottone
+                    }){
+                        Image(systemName: "plus")
+                    }
                 }
-                .navigationTitle("uffa")
+                .padding(.horizontal, 20)
+                Picker(selection: $selectedOption, label: Text("")) {
+                        ForEach(0..<optionsList.count) { index in Text(optionsList[index]).tag(index)
+                            }
+                        }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                NavigationView(){
+                    List {
+                        NavigationLink("Lista 1", destination: Lista_1())
+                        Text("Name")
+                    }
+                }
             }
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Lists")
                 }
-            Text("Second View")
+            //pagina dei prodotti
+            VStack {
+                HStack {
+                    Button(action: {
+                                // Azione del bottone
+                    }){
+                        Image(systemName: "magnifyingglass")
+                    }
+                    .opacity(0)
+                    Spacer()
+                    Button(action: {
+                                // Azione del bottone
+                    }){
+                        Image(systemName: "plus")
+                    }
+                }
+                .padding(.horizontal, 20)
+                Picker(selection: $selectedOption, label: Text("")) {
+                            ForEach(0..<optionsProducts.count) { index in
+                                Text(optionsProducts[index]).tag(index)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                HStack{
+                    TextField("Insert the product name", text: $text)
+                        .padding(.horizontal, 20)
+                }
+                NavigationView(){
+                    List {
+                        NavigationLink("name-code-date", destination: Lista_1())
+                        NavigationLink("name-code-date", destination: Lista_1())
+                        NavigationLink("name-code-date", destination: Lista_1())
+                        Text("Name")
+                    }
+                }
+                Spacer()
+                
+            }
                 .tabItem {
                     Image(systemName: "basket")
                     Text("Products")
                 }
-            Text("Third View")
+            //pagina dei per te
+            VStack{
+                HStack {
+                    Button(action: {
+                                // Azione del bottone
+                    }){
+                        Image(systemName: "magnifyingglass")
+                    }
+                    .opacity(0)
+                    Spacer()
+                    Button(action: {
+                                // Azione del bottone
+                    }){
+                        Image(systemName: "plus")
+                    }
+                }
+                .padding(.horizontal, 20)
+                Picker(selection: $selectedOption, label: Text("")) {
+                        ForEach(0..<optionsForYou.count) { index in Text(optionsForYou[index]).tag(index)
+                            }
+                        }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                Spacer()
+            }
                 .tabItem {
                     Image(systemName: "lightbulb")
                     Text("For You")
                 }
+            //pagina del profilo
             Text("Fourth View")
                 .tabItem {
                     Image(systemName: "person.crop.circle")
